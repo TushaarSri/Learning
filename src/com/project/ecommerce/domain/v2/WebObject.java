899ffca3ac51339bcf4fe384b4883299daaf0e73
Tushaar Srivastava <tushaar.gcsrivastava@gmail.com>
2022-01-25 14:13:48 +0530

    catalog management system

---
+++ src/com/project/ecommerce/domain/v2/WebObject.java
@@ -0,0 +1,8 @@
+package com.project.ecommerce.domain.v2;
+
+/*Interface for all web facing objects*/
+public interface WebObject {
+
+    /*Abstract method declaration, by default public method*/
+    String getUUID();
+}
